9d03bdf7ed00c2ea52b33e4966a799d28d9e75e7
Berkin Koray <73588639+Berkin95@users.noreply.github.com>
2025-05-09 23:48:32 +0200

    Initial Commit

---
+++ OGBody/OGBodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OGBodyApp.swift
+//  OGBody
+//
+//  Created by Berkin Koray Bilgin on 09.05.25.
+//
+
+import SwiftUI
+
+@main
+struct OGBodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
